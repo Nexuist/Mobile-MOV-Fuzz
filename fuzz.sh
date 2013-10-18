0dc381ef3d92de04ffea3f2a927a3e06a34c5846
Nexuist <techboy6601@gmail.com>
2013-10-18 11:27:43 -0400

    Update fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -4,7 +4,7 @@ file=${request_uri:13}
 file_name="${file%.*}"
 if [ -r $file ] && [ $file != "/" ]; then
 seed=$RANDOM
-/opt/local/bin/zzuf -s $seed -r 0.0001:0.001 < $file > ${file_name}_fuzzed.mov
+zzuf -s $seed -r 0.0001:0.001 < $file > ${file_name}_fuzzed.mov
 date +"%T Fuzzed $file with seed $seed" >> log.txt
 echo "Content-Type: video/quicktime"
 echo ""
