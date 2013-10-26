708e6be7af6162801b2cff004149e22571b8e875
Nexuist <techboy6601@gmail.com>
2013-10-26 02:32:01 -0400

    movs are served correctly + simplified script

--- fuzz.sh
+++ fuzz.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-request_uri=$( env | grep REQUEST_URI )
-file=${request_uri:13}
-file_name="${file%.*}"
-if [[ -r $file ]] && [[ $file != "/" ]]; then
+file="Demo"
 seed=$RANDOM
-zzuf -s $seed -r 0.0001:0.001 < $file > ${file_name}_fuzzed.mov
-date +"%T Fuzzed $file with seed $seed" >> log.txt
-echo "Content-Type: video/quicktime"
-echo ""
-cat ${file_name}_fuzzed.mov
-else
-echo "Content-Type: text/plain"
-echo ""
-echo "not found"
-fi
+zzuf -s $seed -r 0.0001:0.001 < $file.mov > fuzzed.mov
+date +"%T Fuzzed $file.mov with seed $seed" >> log.txt
+echo "Content-Type: text/html
+
+<html>
+<head>
+</head>
+<body>
+<video src = "fuzzed.mov" controls autoplay> </video>
+</body>"
